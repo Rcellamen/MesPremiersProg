2979be59a95bf4b4bbdcbb4084d68879ddf724b7
CELLAMEN Rudy <rcellamen@yahoo.fr>
2025-09-03 16:26:01 +0200

    #2 Pseudocode et commentaire d'introduction

--- MesPremiersProg.cpp
+++ MesPremiersProg.cpp
@@ -1,10 +1,26 @@
-// MesPremiersProg.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
+/*Nom du projet : (MesPremiersProg)
+Nom de l'auteur : CELLAMEN
+Date de création : 03/09/2025
+Révison ou version : 16.10.0
+Version du compilateur : 4.8.04084
+Résumé : Programme permettant de choisir une action à effectuer parmi un menu d'options proposé. 
+*/
 
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
 #include <iostream>
+#include <iomanip>
+#include <cmath>
+#define _USE_MATH_DEFINES
+#include <math.h>
+using namespace std;
+using std::pow;
+
+
 
 int main()
 {
-    
+    /* */
 }
 
